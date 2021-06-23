affb3fc89641bb5a3d847801e4456ee5a0de39b5
rildodev <rildombs@gmail.com>
2021-06-22 22:02:26 -0300

    first commit

---
+++ Aula 01 -  O começo de tudo/main.js
@@ -0,0 +1,44 @@
+/* Função para abrir/fechar o menu ao clicar sobre o icone */
+const nav = document.querySelector('#header nav')
+const toggle = document.querySelectorAll('nav .toggle')
+
+for (const element of toggle) {
+  element.addEventListener('click', function () {
+    nav.classList.toggle('show')
+  })
+}
+
+/* Esconde menu quando algum opção é clicada */
+const links = document.querySelectorAll('nav ul li a')
+
+for (const link of links) {
+  link.addEventListener('click', function () {
+    nav.classList.remove('show')
+  })
+}
+
+/* muda o header da página quando der scroll */
+const header = document.querySelector('#header')
+const navHeight = header.offsetHeight
+
+window.addEventListener('scroll', function () {
+  if (window.scrollY >= navHeight) {
+    // scroll é maior que a altura do header
+    header.classList.add('scroll')
+  } else {
+    //menor que a altura do header
+    header.classList.remove('scroll')
+  }
+})
+
+/* TESTIMONIAIS - CARROSEL (SWIPER JS) */
+
+const swiper = new Swiper('.swiper-container', {
+  slidesPerView: 1,
+  pagination: {
+    el: '.swiper-pagination'
+  },
+  mousewheel: true,
+  keyboard: true,
+  
+})
